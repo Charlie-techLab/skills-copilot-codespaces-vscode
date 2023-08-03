b790ebc3647c7c261dcf98badcb6eaa392f371f7
Charlie <95556239+Charlie-techLab@users.noreply.github.com>
2023-08-03 23:35:46 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,88 @@
+// Create web server application with Node.js and Express.js
+// Load the express module
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Load the comments module
+const comments = require('./comments');
+
+// Use the comments module
+app.use('/comments', comments);
+
+// Start the server
+app.listen(port, () => console.log(`Server is listening on port: ${port}`));
+
+// Path: comments.js
+// Create web server application with Node.js and Express.js
+// Load the express module
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Load the comments module
+const comments = require('./comments');
+
+// Use the comments module
+app.use('/comments', comments);
+
+// Start the server
+app.listen(port, () => console.log(`Server is listening on port: ${port}`));
+
+// Path: comments.js
+// Create web server application with Node.js and Express.js
+// Load the express module
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Load the comments module
+const comments = require('./comments');
+
+// Use the comments module
+app.use('/comments', comments);
+
+// Start the server
+app.listen(port, () => console.log(`Server is listening on port: ${port}`));
+
+// Path: comments.js
+// Create web server application with Node.js and Express.js
+// Load the express module
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Load the comments module
+const comments = require('./comments');
+
+// Use the comments module
+app.use('/comments', comments);
+
+// Start the server
+app.listen(port, () => console.log(`Server is listening on port: ${port}`));
+
+// Path: comments.js
+// Create web server application with Node.js and Express.js
+// Load the express module
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Load the comments module
+const comments = require('./comments');
+
+// Use the comments module
+app.use('/comments', comments);
+
+// Start the server
+app.listen(port, () => console.log(`Server is listening on port: ${port}`));
+
+// Path: comments.js
+// Create web server application with Node.js and Express.js
+// Load the express module
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Load the comments module
+const comments
